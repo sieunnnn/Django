b6dc6acc10c9939b84142407052aea3ee571061e
sieunnnn <wldsmtldsm65@gmail.com>
2023-02-02 19:24:21 +0900

    [Do it!] 데이 2 | 장고 기초 알아보기

---
+++ DoIt/projects/mysite/pybo/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def index(request) :
+    return HttpResponse("안녕하세요 pybo 에 오신것을 환영합니다."
+                        )
